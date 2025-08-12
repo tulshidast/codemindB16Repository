6d1db9cdb3cf3a2b4ec2d53a89bb5fc47025978e
tulshidast <tulsidast77@gmail.com>
2025-08-12 07:47:41 +0530

    changes

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -8,28 +8,28 @@ import com.aventstack.extentreports.Status;
 
 public class TestListener implements ITestListener {
 
-	@Override
-	public void onTestStart(ITestResult result) {
-		ExtentTestManager.startTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
-	}
-
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		ExtentTestManager.getTest().log(Status.PASS, "Test Passed");
-	}
-
-	@Override
-	public void onTestFailure(ITestResult result) {
-		ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + result.getThrowable());
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult result) {
-		ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped: " + result.getThrowable());
-	}
-
-	@Override
-	public void onFinish(ITestContext context) {
-		ExtentTestManager.endTest();
-	}
+//	@Override
+//	public void onTestStart(ITestResult result) {
+//		ExtentTestManager.startTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
+//	}
+//
+//	@Override
+//	public void onTestSuccess(ITestResult result) {
+//		ExtentTestManager.getTest().log(Status.PASS, "Test Passed");
+//	}
+//
+//	@Override
+//	public void onTestFailure(ITestResult result) {
+//		ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + result.getThrowable());
+//	}
+//
+//	@Override
+//	public void onTestSkipped(ITestResult result) {
+//		ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped: " + result.getThrowable());
+//	}
+//
+//	@Override
+//	public void onFinish(ITestContext context) {
+//		ExtentTestManager.endTest();
+//	}
 }
