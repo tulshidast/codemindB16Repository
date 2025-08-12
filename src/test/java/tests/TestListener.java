69ad97ddb0744c7a7f46848a6ee079885bddd0b9
tulshidast <tulsidast77@gmail.com>
2025-08-12 07:50:10 +0530

    changes

---
+++ src/test/java/tests/TestListener.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.Status;
+
+import utils.ExtentTestManager;
+
+public class TestListener implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		ExtentTestManager.startTest(result.getMethod().getMethodName(), result.getMethod().getDescription());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		ExtentTestManager.getTest().log(Status.PASS, "Test Passed");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + result.getThrowable());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped: " + result.getThrowable());
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		ExtentTestManager.endTest();
+	}
+}
